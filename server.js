05f184d23622729489242e87300132e857caabb1
Nixon Duong <nixonduong@Nixons-MacBook-Pro.local>
2019-01-06 17:54:14 -0800

    updated server to handle get data

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ var app = express();
 var url = "mongodb://username:password@ds115340.mlab.com:15340/userlist";
 var port = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/get-data', (req, res) => {
+	var data = [];
+	mongo.connect(url, (err, db) => {
+		assert.equal(null, err);
+		var cursor = db.collection('item-data').find();
+		cursor.forEach((doc, err) => {
+			assert.equal(null, err);
+			data.push(doc);
+		}, function(){
+			db.close();
+			res.json(data);
+		});
+	});
+});
+
 app.post('/submit', (req, res) => {
 	var item = {
 		item: req.body.item,
